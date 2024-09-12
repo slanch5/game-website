41366659758aeaa06bb546aeaced133f07bbbe89
vlacheslav katerynych <bvelorant@gmail.com>
2024-09-12 10:52:47 +0300

    add slider

--- website_game_main_dir/app/js/main.js
+++ website_game_main_dir/app/js/main.js
@@ -22,3 +22,19 @@ $(function () {
     
 
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+   const swiper = new Swiper('.swiper', {
+    
+     loop: true,
+   //   pagination: {
+   //     el: '.swiper-pagination',
+       
+   //   },
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+     },
+     
+   });
+ });
